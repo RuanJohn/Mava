9eb562bdd80663600a1b0260bc5e640432cc26de
Ruan de Kock <ruanjdekock@gmail.com>
2024-08-11 19:58:04 +0200

    feat: add correct system names to tabular policy logger configs

--- mava/systems/ppo/ff_ppo_central_tabular.py
+++ mava/systems/ppo/ff_ppo_central_tabular.py
@@ -476,7 +476,9 @@ def run_experiment(_config: DictConfig) -> float:
 
 
 @hydra.main(
-    config_path="../../configs", config_name="default_ff_ppo_central.yaml", version_base="1.2"
+    config_path="../../configs",
+    config_name="default_ff_ppo_central_tabular.yaml",
+    version_base="1.2",
 )
 def hydra_entry_point(cfg: DictConfig) -> float:
     """Experiment entry point."""
