1563d7f330827618cc663730d7b899b344b35898
Ruan de Kock <ruanjdekock@gmail.com>
2025-01-11 19:57:51 +0200

    chore: update list of submitted tasks

--- launch_sweeps.py
+++ launch_sweeps.py
@@ -89,6 +89,7 @@ systems_to_run = [
 
 system_scenarios_to_skip = {
     "rec_ppo_central_factrored": [
+        # don't have to run cont tasks
         "simple_spread_3ag",
         "simple_spread_5ag",
         "simple_spread_10ag",
@@ -97,6 +98,7 @@ system_scenarios_to_skip = {
         "hopper_3x1",
         "humanoid_9|8",
         "walker2d_2x3",
+        # these have already been submitted
         "2s-8x8-2p-2f-coop",
         "8x8-2p-2f-coop",
         "2s-10x10-3p-3f",
@@ -111,6 +113,22 @@ system_scenarios_to_skip = {
         "27m_vs_30m",
         "3s5z_vs_3s6z",
         "3s_vs_5z",
+        "6h_vs_8z",
+        "smacv2_5_units",
+        "smacv2_10_units",
+        "smacv2_20_units",
+        "tiny-2ag",
+        "tiny-4ag",
+        "small-4ag",
+        "tiny-2ag-hard",
+        "tiny-4ag-hard",
+        "small-4ag-hard",
+        "medium-6ag",
+        "large-8ag",
+        "large-8ag-hard",
+        "medium-4ag",
+        "medium-4ag-hard",
+        "large-4ag",
     ],
     "rec_ppo_central": [
         "con-15x15x23a",
