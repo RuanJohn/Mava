04f13d7fcb8c0a17e553a4ea0d8794c286c5e2fc
Ruan de Kock <ruanjdekock@gmail.com>
2025-12-23 17:41:11 +0200

    feat: final setup to rerun climbing on all systems

--- run_climb.sh
+++ run_climb.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 env=matrax
-python mava/systems_tuning/mat/anakin/mat.py -m env=${env} &&\
-python mava/systems_tuning/sable/anakin/ff_sable.py -m env=${env} &&\
-python mava/systems_tuning/ppo/anakin/ff_ppo_central.py -m env=${env} &&\
-python mava/systems_tuning/ppo/anakin/ff_ppo_central_tabular.py -m env=${env} &&\
-python mava/systems_tuning/ppo/anakin/ff_mappo.py -m env=${env} &&\
-python mava/systems_tuning/ppo/anakin/ff_ippo.py -m env=${env} &&\
-python mava/systems_tuning/ppo/anakin/ff_ppo_central_factored.py -m env=${env} &&\
-python mava/systems_tuning/ppo/anakin/ff_ippo_tabular_split.py -m env=${env}
+python mava/systems/mat/anakin/mat.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/sable/anakin/ff_sable.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/ppo/anakin/ff_ppo_central.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/ppo/anakin/ff_ppo_central_tabular.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/ppo/anakin/ff_mappo.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/ppo/anakin/ff_ippo.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/ppo/anakin/ff_ppo_central_factored.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/ppo/anakin/ff_ippo_tabular_split.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9 &&\
+python mava/systems/ppo/anakin/ff_ppo_central_factored_tabular.py -m env=${env} system.seed=0,1,2,3,4,5,6,7,8,9
